ab6394c8663dafe10630892a5a52662527fa5895
rashmikondaparthi <100505034+rashmikondaparthi@users.noreply.github.com>
2023-05-07 10:17:38 +0530

    Update Callable.java

--- Callable.java
+++ Callable.java
@@ -3,12 +3,7 @@ public class Callable
 {
 	public static void main(String args[]) throws Exception
 	{
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
-		CallableStatement stmt = con.prepareCall("{call INSERT(?,?)}");
-		stmt.setString(1,"keerthana");
-		stmt.setString(2,"HYD");
-		stmt.executeUpdate();
+		System.out.println("helloworld");
 		
 	}
 }
